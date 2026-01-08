7291de921ffd4505c7c0d2cd51ba8a5b8d2c52ef
davidcllm <davcm24@hotmail.com>
2026-01-08 17:42:56 -0600

    Creación de la barra de reproducción.
    Signed-off-by: davidcllm <davcm24@hotmail.com>

--- spotifyClone/src/components/Sidebar.jsx
+++ spotifyClone/src/components/Sidebar.jsx
@@ -36,7 +36,7 @@ const Sidebar = () => {
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-star gap-1 pl-4 mt-4'>
                 <h1>Let's find dome podcast to follow</h1>
                 <p className='font-light'>We'll keep you updated on new episodes</p>
-                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse podcastsS</button>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse podcasts</button>
             </div>
 
         </div>

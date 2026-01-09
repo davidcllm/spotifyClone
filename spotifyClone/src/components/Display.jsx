a40d7ef53030b2bfb2cd37dba7b1a91416176aec
davidcllm <davcm24@hotmail.com>
2026-01-08 22:28:09 -0600

    Corrección en enrutamiento y nuevos botones.
    Signed-off-by: davidcllm <davcm24@hotmail.com>

--- spotifyClone/src/components/Display.jsx
+++ spotifyClone/src/components/Display.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import DisplayHome from './DisplayHome';
 
 // pegar comando "npm install react-router-dom" en terminal para enrutamiento
 const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
-            <Route path='/' elements={<DisplayHome/>} />
+            <Route path='/' element={<DisplayHome/>}/>
         </Routes>
     </div>
   );
